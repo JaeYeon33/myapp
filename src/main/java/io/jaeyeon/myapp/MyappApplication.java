b1c3b3c50847e3ccefe2b54514a11af77d54aa30
JaeYeon33 <cjyeon1022@gmail.com>
2021-06-22 23:24:50 +0900

    2021-06-22 Change JpaAuditing file

--- src/main/java/io/jaeyeon/myapp/MyappApplication.java
+++ src/main/java/io/jaeyeon/myapp/MyappApplication.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import java.util.Optional;
 import java.util.UUID;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing
 @SpringBootApplication
 public class MyappApplication {
 
--- src/main/java/io/jaeyeon/myapp/controller/IndexController.java
+++ src/main/java/io/jaeyeon/myapp/controller/IndexController.java
@@ -30,17 +30,14 @@ public class IndexController {
     private final PostsService postsService;
     private final HttpSession httpSession;
 
-    // Model 객체
-    // 서버 탬플릿 엔진에서 사용가능한 객체를 저장할 수 있다.
-    // 해당 메소드에서는 postsService.findAllDesc()로 가져온 결과를 posts라는 이름으로 index.mustache에 전달한다.
     @GetMapping("/")
     public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
-        // 세션에 저장된 값이 있을 경우만 model에 userName으로 등록한다.
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
+
         return "index";
     }
 
@@ -49,18 +46,12 @@ public class IndexController {
         return "posts-save";
     }
 
-
-    @PostMapping("/posts/save")
-    public String postsWrite(PostsSaveRequestDto requestDto) {
-        postsService.save(requestDto);
-        return "redirect:/";
-    }
-
-
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
+
         return "posts-update";
     }
+
 }
